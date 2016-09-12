955d5b04b233b7cb67ffad3ab1356b57330163ee
HONOKA\honoka <2841003@bemax.jp>
2016-09-12 12:54:48 +0900

    やっとできた

--- codeiq2.rb
+++ codeiq2.rb
@@ -1,5 +1,6 @@
-@args = []
+@args = [[1,5],[2,3]]
 i = 0
+
 while line = gets
 	ary = line.split("/")
 	@args[i] = []
@@ -9,4 +10,46 @@ while line = gets
 	break if i==2
 end
 
-p @args
+
+def gcd(a,b)
+	a,b=b,a%b while b>0
+	a
+end
+
+a=@args[0][1]
+b=@args[1][1]
+if b>a
+   w=a
+   a=b
+   b=w
+end
+
+g=gcd(a,b) #<-Å‘åŒö–ñ”
+l=g*(a/g)*(b/g) #<-Å¬Œö”{”
+
+@args[0][0]=@args[0][0]*l/@args[0][1]
+@args[0][1]=l
+@args[1][0]=@args[1][0]*l/@args[1][1]
+@args[1][1]=l
+
+
+
+bunsi=@args[0][0]+@args[1][0]
+bunbo=l
+
+
+
+if bunsi>bunbo
+	g=gcd(bunsi,bunbo)
+else
+	g=gcd(bunbo,bunsi)
+end
+
+bunsi=bunsi/g
+bunbo=bunbo/g
+
+if bunbo==1
+	puts bunsi
+else
+	puts "#{bunsi}/#{bunbo}"
+end
